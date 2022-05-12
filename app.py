3aabc2822f6843e2adaf63534a29d2f80c57244e
Namya LG <namyalg@gmail.com>
2022-05-12 09:25:49 +0530

    update table schema

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from config_table import rename_columns
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Web App with Python Flask!'
@@ -10,7 +11,10 @@ def index():
 
 @app.route('/config', methods=['POST'])
 def config_table():
-    return {'response' : rename_columns(request.get_json())}
+    try:
+        return {'status': 1, 'response': rename_columns(request.get_json())}
+    except Exception:
+        return {'status': 0, 'error': Exception}
 
 
 if __name__ == "__main__":
